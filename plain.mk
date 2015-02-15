b5c16f512a5b00ead19ff543206b648bc90ecf05
Snuzzo <jrracinfan@gmail.com>
2015-02-15 05:17:43 -0500

    Plain-ify

---
+++ plain.mk
@@ -0,0 +1,6 @@
+$(call inherit-product, device/htc/m8/full_m8.mk)
+$(call inherit-product, vendor/plain/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/plain/config/common.mk)
+#$(call inherit-product, vendor/deso/config/telephony.mk)
+
+PRODUCT_NAME := plain_m8
